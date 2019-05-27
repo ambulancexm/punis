ca8d6eb32e4e9246a77392dc3ac123911cca9c02
Pauline <pauline@ldnr.fr>
2019-05-27 15:29:23 +0200

    test affichage listePunissemnt

---
+++ app/src/main/java/com/example/punissementmanager/MyViewHolder.java
@@ -0,0 +1,16 @@
+package com.example.punissementmanager;
+
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import com.objetsjava.Punissement;
+
+class MyViewHolder extends RecyclerView.ViewHolder {
+    public MyViewHolder(@NonNull View itemView) {
+        super(itemView);
+    }
+
+    public void bind(Punissement punissementObject) {
+    }
+}
